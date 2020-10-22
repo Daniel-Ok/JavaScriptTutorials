fddcf6c0cb48d4ad1d91512690b91cbcba6b5a23
Daniel-Ok <daniel.okyere@amalitech.org>
2020-10-22 15:41:31 +0000

    changed result description and worked on strings

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,5 +1,6 @@
 let currentResults = 0;
 
-currentResults = currentResults + 10;
+currentResults = (currentResults + 10)*3/2-1;
+let calculationDescription = '(' + currentResults + '+ 10)'; 
 
-outputResult(currentResults,'');
+outputResult(currentResults,calculationDescription);
