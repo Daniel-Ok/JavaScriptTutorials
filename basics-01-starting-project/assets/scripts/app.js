df053f88fc2512b45ef56468b071c9ff65e31e01
Daniel-Ok <daniel.okyere@amalitech.org>
2020-10-24 09:41:33 +0000

    split code into functions

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -2,8 +2,9 @@ const defaultResult = 0;
 let currentResults = defaultResult;//initialise with another variable
 
 function add(){
+    const calcDescription = `${currentResults} + ${userInput.value}`;
     currentResults = currentResults + parseInt(userInput.value);
-    outputResult(currentResults, '');
+    outputResult(currentResults, calcDescription );
 }
 addBtn.addEventListener('click', add);
 
