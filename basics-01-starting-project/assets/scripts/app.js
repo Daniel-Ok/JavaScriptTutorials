2270e499233ea3131136d694aa215f8e569fe4b5
Daniel-Ok <daniel.okyere@amalitech.org>
2020-10-24 06:05:39 +0000

    more on functions

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -3,11 +3,11 @@ let currentResults = defaultResult;//initialise with another variable
 
 function add(num1, num2){
     const result = num1 + num2;
-    alert('The result is ' + result);
+    return result;
 }
-add(1,2);
-add(3,5);
-currentResults = (currentResults + 10) * 3 / 2 - 1;
+currentResults = add(1,2);
+
+
 let calculationDescription = '(' + defaultResult + '+ 10)'; 
 
 outputResult(currentResults,calculationDescription);
