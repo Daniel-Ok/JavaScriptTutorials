17f264c5e361a84866e8aef3720b671f1721ae69
Daniel-Ok <daniel.okyere@amalitech.org>
2020-10-25 12:01:30 +0000

    working unconventional calculator

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,11 +1,44 @@
 const defaultResult = 0;
 let currentResults = defaultResult;//initialise with another variable
 
-function add(){
-    const calcDescription = `${currentResults} + ${userInput.value}`;
-    currentResults = currentResults + parseInt(userInput.value);
-    outputResult(currentResults, calcDescription );
+function createAndWriteOutput(operator, resultBeforeCalc, CalcNum){
+    const calcDescription = `${resultBeforeCalc} ${operator} ${CalcNum}`;
+    outputResult(currentResults, calcDescription)
 }
+
+function getUserNumInput(){
+    return parseInt(userInput.value);
+}
+function add(){
+    const enteredNum = getUserNumInput();
+    const initialRes = currentResults;
+    currentResults = currentResults + enteredNum;
+    createAndWriteOutput('+', initialRes, enteredNum);
+}
+
+
+
+function subtract(){
+    const enteredNum = getUserNumInput();
+    const initialRes = currentResults;
+    currentResults = currentResults - enteredNum;
+    createAndWriteOutput('-', initialRes, enteredNum);
+}
+
+function multiply(){
+    const enteredNum = getUserNumInput();
+    const initialRes = currentResults;
+    currentResults = currentResults*enteredNum;
+    createAndWriteOutput('*', initialRes, enteredNum);
+}
+function divide(){
+    const enteredNum = getUserNumInput();
+    const initialRes = currentResults;
+    currentResults = currentResults/enteredNum;
+    createAndWriteOutput('/', initialRes, enteredNum);
+}
+
 addBtn.addEventListener('click', add);
-
-
+subtractBtn.addEventListener('click',subtract);
+multiplyBtn.addEventListener('click',multiply);
+divideBtn.addEventListener('click', divide);
