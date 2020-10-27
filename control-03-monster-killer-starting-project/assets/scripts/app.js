870e56ee03bd78d2988b3ea39f8f2df2c16c1b79
Daniel-Ok <daniel.okyere@amalitech.org>
2020-10-27 23:44:19 +0000

    added strong attack

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -3,6 +3,7 @@ const ATTACK_VALUE = 10;
 let currentMonsterHealth = chosenMaxLife;
 const MONSTER_ATTACK_VALUE = 14;
 let currentPlayerHealth =100;
+const STRONG_ATTACK_VALUE = 14;
 
 adjustHealthBars(chosenMaxLife);
 
@@ -20,4 +21,18 @@ function attackHandler(){
   }
 }
 
+function strongAttackHandler(){
+    const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
+    currentMonsterHealth -= damage;
+      const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
+      currentPlayerHealth -= playerDamage;
+    if(currentMonsterHealth <= 0 && currentPlayerHealth > 0){
+        alert('You Won!');
+    }else if(currentPlayerHealth <= 0 && currentMonsterHealth > 0){
+        alert('You Lost!');
+    }else if(currentPlayerHealth<=0 && currentMonsterHealth<=0){
+        alert('Draw');
+    }
+}
 attackBtn.addEventListener('click', attackHandler)
+strongAttackBtn.addEventListener('click',strongAttackHandler)
