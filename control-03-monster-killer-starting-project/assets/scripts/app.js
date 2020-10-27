ac5070d6dc695832578b80a79cc6d0cde1368850
Daniel-Ok <daniel.okyere@amalitech.org>
2020-10-27 23:23:57 +0000

    added attack details and reduces monster health on click

---
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -0,0 +1,12 @@
+let  chosenMaxLife = 100;
+const ATTACK_VALUE = 10;
+let currentMonsterHealth = chosenMaxLife;
+
+adjustHealthBars(chosenMaxLife);
+
+function attackHandler(){
+  const damage = dealMonsterDamage(ATTACK_VALUE);
+  currentMonsterHealth -= damage;
+}
+
+attackBtn.addEventListener('click', attackHandler)
