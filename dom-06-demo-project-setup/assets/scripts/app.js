c4440e1609f29f44e9ec3b04638dc6edc17cdcd4
Daniel-Ok <daniel.okyere@amalitech.org>
2020-11-02 11:14:47 +0000

    improvements on the backdrop

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -3,7 +3,7 @@ const addMovieModal = document.getElementById('add-modal');
 
 const startAddMovieBtn = document.querySelector('header button');
 const backdrop = document.getElementById('backdrop');
-
+const cancelAddMovieBtn = addMovieModal.querySelector('.btn--passive');
 
 const toggleMovieModal = () =>{ //function(){}
     addMovieModal.classList.toggle('visible');
@@ -16,6 +16,9 @@ const toggleBackdrop = ()=>{
 const backdropClickHandler = () =>{
     toggleMovieModal()
 }
-
+const cancelAddMovie = () =>{
+    toggleMovieModal();
+};
 startAddMovieBtn.addEventListener('click', toggleMovieModal);
 backdrop.addEventListener('click', toggleMovieModal);
+cancelAddMovieBtn.addEventListener('click',cancelAddMovie);
