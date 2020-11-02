541c12eb3866162a8bdd3a8ac119e8f42be8e91b
Daniel-Ok <daniel.okyere@amalitech.org>
2020-11-02 11:05:27 +0000

    changed the backdrop

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -2,9 +2,20 @@ const addMovieModal = document.getElementById('add-modal');
 //const addMovieModal = document.querySelector('#add-modal');
 
 const startAddMovieBtn = document.querySelector('header button');
+const backdrop = document.getElementById('backdrop');
+
 
 const toggleMovieModal = () =>{ //function(){}
     addMovieModal.classList.toggle('visible');
+    toggleBackdrop();
 };
 
+const toggleBackdrop = ()=>{
+    backdrop.classList.toggle('visible');
+}
+const backdropClickHandler = () =>{
+    toggleMovieModal()
+}
+
 startAddMovieBtn.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', toggleMovieModal);
